b9e7a4db82d439c3c7a50a24700e8b94bfbe9e59
AndrewJLang <andrewespn@hotmail.com>
2021-03-16 15:01:00 -0700

    reverted back to supported version for node-hue-api

---
+++ dist/cjs/index.d.ts
@@ -0,0 +1,6 @@
+import * as model from "./model";
+export { model };
+import * as time from "./time";
+export { time };
+import * as types from "./types";
+export { types };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-export * as types from './types/index';
-export * as model from './model/index';
-export * as timePatterns from './timePatterns/index'
-
-export * as v3Model from './model/v3';
+import * as types from "./types/index";
+import * as model from "./model/index";
+import * as timePatterns from "./timePatterns/index";
+import * as v3Model from "./model/v3";
+export { types };
+export { model };
+export { timePatterns };
+export { v3Model };
